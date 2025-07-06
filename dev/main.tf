caa999f013df80736afc2d9e8729fed3dbb724df
Your Name <you@example.com>
2025-07-06 22:15:13 +0530

    new rg

--- dev/main.tf
+++ dev/main.tf
@@ -2,12 +2,12 @@ module "md-rg" {
   source = "../../module/azure-rg"
   rg     = var.prg
 }
-module "md-rg1" {
+module "md-rg3" {
   source = "../../module/azure-rg"
   rg     = var.prg
 }
 
-nishant 
+ 
 
 module "md-vnet" {
   source     = "../../module/azure-vnet"
